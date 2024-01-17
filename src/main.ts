ff8b4a96b4bc0107f981b816bef8b9c808f8a971
Prateek Koirala <prateekkoirala77@gmail.com>
2024-01-17 12:24:34 +0545

    Union Type

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ let myName: string = 'Prateek'
 let meaningOfLife: number;
 let isLoading: boolean;
 let album: any;
+let singer: string | number;
 
 myName = 'Prateek'
 meaningOfLife = 45
 isLoading = true 
 album = true 
+singer = 'prateek'
+
+const sum = (a: number,b: number) => {  
+    return a+b
+}
