26d330fc64610545159f645dd663a3e94b057f4a
Prateek Koirala <prateekkoirala77@gmail.com>
2024-01-18 13:22:24 +0545

    'object'

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,50 @@
-// let a:number;
-// let b:number;
-declare type SumProps = {
-    a:number,
-    b:number
-}
-function sum (props:SumProps) {
-    const {
-        a,b
-    }
-    = props
-    return a+b
+let bands: string[]=[]
+let myObj: object 
+myObj = []
+console.log(typeof myObj)
+myObj = bands
+myObj = {}
+
+
+const exampleObj = {
+    prop1: 'Prateek',
+    prop2: true,
 }
 
-console.log(sum({a:5,b:10}));
+exampleObj.prop1 = 'Pramit'
+
+type Guitarist = {
+    name : string,
+    active : boolean,
+    albums : (string|number)[]
+}
+
+let evh: Guitarist={
+    name:'Eddie',
+    active:false,
+    albums:[1984,5150,'OU812']
+}
+
+let jp: Guitarist = {
+    name: 'Jimmy',
+    active:false,
+    albums:['I','II','IV']
+}
+
+const greetGuitarist = (guitarist: Guitarist) => {
+    return `Hello ${guitarist.name}!`
+}
+console.log(greetGuitarist(jp))
+
+
+enum Grade {
+    U =1,
+    D,
+    C,
+    B,
+    A,
+}
+
+ console.log(Grade.U)
+
+ 
