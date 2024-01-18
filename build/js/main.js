dc9edd187ec1a89a2edaef0f16447672552e3b3e
Prateek Koirala <prateekkoirala77@gmail.com>
2024-01-18 16:44:52 +0545

    'Function'

--- build/js/main.js
+++ build/js/main.js
@@ -3,3 +3,61 @@
 let myName;
 let userName;
 userName = 'Abhiyan';
+// Function 
+const add = (a, b) => {
+    return a + b;
+};
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg('Hello!');
+logMsg(add(2, 3));
+let subtract = function (c, d) {
+    return c - d;
+};
+let multiply = function (c, d) {
+    return c * d;
+};
+logMsg(multiply(2, 2));
+//optional parameters 
+const addAll = (a, b, c) => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
+    }
+    return a + b;
+};
+const sumAll = (a = 10, b, c = 2) => {
+    return a + b;
+};
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(undefined, 3));
+//Rest Parameter 
+const total = (a, ...nums) => {
+    return a + nums.reduce((prev, curr) => prev + curr);
+};
+logMsg(total(1, 2, 3, 4));
+const createError = (errMsg) => {
+    throw new Error(errMsg);
+};
+const infinte = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        if (i > 100)
+            break;
+    }
+};
+//custom type guard
+const isNumber = (value) => {
+    return typeof value == 'number'
+        ? true : false;
+};
+//use of the never type
+const numberOrString = (value) => {
+    if (typeof value == 'string')
+        return 'string';
+    if (isNumber(value))
+        return 'number';
+    return createError('This should never happen!');
+};
--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,87 @@ let myName: 'Prateek'
 
 let userName : 'Prateek' | 'Sujar' | 'Abhiyan'
 userName = 'Abhiyan'
+
+// Function 
+ const add = (a: number, b: number): number => {
+    return a+b 
+ }
+  const logMsg = (message:  any): void => {
+    console.log(message)
+  }
+
+  logMsg('Hello!')
+  logMsg(add(2,3))
+
+  let subtract = function (c: number, d: number): 
+  number{
+    return c-d
+  }
+
+//   type mathFunction = (a: number,b: number)=> number
+  interface mathFunction {
+    (a: number,b: number): number
+  } 
+
+  let multiply: mathFunction = function (c,d){
+    return c*d
+  }
+  
+  logMsg(multiply(2,2))
+
+  //optional parameters 
+  const addAll = (a: number,b: number,c?: number):
+  number => {
+    if (typeof c!== 'undefined'){
+         return a+b+c
+
+    }
+    return a+b 
+   
+  }
+
+   const sumAll = (a: number =10,b: number,c: number=2):
+  number => {
+    return a+b 
+   
+  }
+
+  logMsg(addAll(2,3,2))
+   logMsg(addAll(2,3))
+    logMsg(sumAll(undefined,3))
+
+    //Rest Parameter 
+
+    const total = (a: number, ...nums: number[]): number => {
+        return a + nums.reduce((prev,curr)=>prev + curr)
+    }
+
+    logMsg(total(1,2,3,4))
+
+
+    const createError = (errMsg: string): never => {
+        throw new Error(errMsg)
+    }
+
+    const infinte = () => {
+        let i: number = 1
+        while (true){
+            i++
+            if(i>100)break
+        }
+    }
+
+    //custom type guard
+    const isNumber = (value: any): boolean => {
+        return typeof value == 'number'
+        ?true : false
+    }
+
+
+     //use of the never type
+    const numberOrString = (value: number | string):
+    string => {
+        if (typeof value == 'string')return 'string'
+        if (isNumber(value))return 'number'
+        return createError('This should never happen!')
+    }
