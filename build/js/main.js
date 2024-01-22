a2bfb44711b21373c802152b91af968e443ae5c4
Prateek Koirala <prateekkoirala77@gmail.com>
2024-01-22 11:45:46 +0545

    'GS'

--- build/js/main.js
+++ build/js/main.js
@@ -1,23 +1,75 @@
 "use strict";
-//convert to more or less specific 
-let a = 'hello';
-let b = a; //less specific 
-let c = a; //more specific 
-let d = 'world';
-let e = 'world';
-const addOrConcat = (a, b, c) => {
-    if (c == 'add')
-        return a + b;
-    return '' + a + b;
-};
-let myVal = addOrConcat(2, 2, 'concat');
-//Be careful! TS sees no problem -but a string is returned  
-let nextVal = addOrConcat(2, 2, 'concat');
-// 10 as string 
-10;
-// The DOM 
-const img = document.querySelector('img');
-const myImg = document.getElementById('#img');
-const nextImg = document.getElementById('#img');
-img.src;
-myImg.src;
+// class Coder {
+//   name: string 
+//   music: string
+//   age: number
+//   lang: string
+//   constructor(
+//     name: string,
+//     music: string,
+//     age:number,
+//     lang:string
+//     ){
+//     this.name = name 
+//     this.music = music 
+//     this.age = age
+//     this.lang = lang
+//   }
+// }
+//Class
+class Coder {
+    constructor(name, music, age, lang = 'Typescript') {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    getAge() {
+        return `Hello,I'm ${this.age}`;
+    }
+}
+const Prateek = new Coder('Prateek', 'Rock', 23);
+console.log(Prateek.getAge());
+class WebDev extends Coder {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+const Sara = new WebDev('Mac', 'Sara', 'Lofi', 25);
+console.log(Sara.getLang());
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const Page = new Guitarist('Jimmy', 'guitar');
+console.log(Page.play('strums'));
+//////////////////////////////////////////////////////////
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const John = new Peeps('John');
+const Steve = new Peeps('Steve');
+const Amy = new Peeps('Amy');
+console.log(Peeps.count);
